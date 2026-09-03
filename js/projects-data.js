71b2feb1603e049cb1a6d19b494b613a430dad5e
Tiev84 <tievdesigner@gmail.com>
2026-09-03 22:46:31 +0700

    Cập nhật ảnh portfolio

--- js/projects-data.js
+++ js/projects-data.js
@@ -442,10 +442,12 @@ window.PROJECTS = {
     title: "GOODDY BAR - ĐỨC",
     description: "GOODDY Restaurant & Bar là một góc ẩm thực và giải trí ấm cúng tại Hamburg, Đức, nổi bật với không gian nhỏ xinh, mang tinh thần phóng khoáng nhưng vẫn rất gần gũi. Đồng hành cùng dự án từ những ngày đầu, Tiến trực tiếp sáng tạo Logo nhận diện và Poster khai trương cho quán. Thiết kế tập trung vào sự tinh tế, cô đọng, vừa lột tả trọn vẹn cái \"vibe\" ấm áp, chill chill của một không gian bar thu nhỏ, vừa tạo điểm nhấn ấn tượng để thu hút thực khách ngay từ ngày đầu mở cửa.",
     images: [
-      "assets/project/PROJECT MOI 7/526419275_122118541322932701_3827701155108702371_n.png",
+      "assets/project/PROJECT MOI 7/thumb bar.png",
       "assets/project/PROJECT MOI 7/GOODY LOGO PNG.png",
-      "assets/project/PROJECT MOI 7/528327068_122119895948932701_6307129062458487922_n.jpg",
       "assets/project/PROJECT MOI 7/poster bar.png",
+      "assets/project/PROJECT MOI 7/528327068_122119895948932701_6307129062458487922_n.jpg",
+      "assets/project/PROJECT MOI 7/519095977_122114488316932701_2564112330347380035_n.jpg",
+      "assets/project/PROJECT MOI 7/518196310_122107122302932701_1557275460819053808_n.jpg",
     ],
   },
   "project-moi-8": {
